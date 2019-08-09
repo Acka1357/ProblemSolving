548bc92b49386142db300f43f5b4ae78c6a9fb47
Acka1357 <Acka1357@gmail.com>
2019-08-09 22:05:23 +0900

    Educational Codeforces Round 70

--- Codeforces/E70_D2/E.cpp
+++ Codeforces/E70_D2/E.cpp
@@ -3,29 +3,91 @@
 #include <vector>
 #include <cstring>
 #include <queue>
-#include <utility>
-#include <functional>
-#include <cstdlib>
-#include <cmath>
-#include <string>
-#include <set>
-#include <map>
 using namespace std;
 
 typedef long long ll;
 
-struct Point{ ll x, y;
-	bool operator <(const Point &o) const{ return x == o.x ? y < o.y : x < o.x; }
-};
-struct Crd{ int r, c; };
-struct Edge{ int to, cost; };
-struct Node{ int cur; ll cost;
-	bool operator <(const Node &o)const{ return cost > o.cost; }
+struct Node{
+	int idx, cnt;
+	Node *nxt[26], *fail;
 };
 
+int nidx = 2, L;
+Node nodes[400002];
+ll st[200001], ed[200001];
+
+void add(char *s, Node *cur, int si, int di){
+	for(int i = 0, val; s[i]; i++, si += di){
+		val = s[i] - 'a';
+		if(!(cur->nxt[val])){
+			nodes[nidx].idx = si;
+			cur->nxt[val] = &nodes[nidx++];
+		}
+		cur = cur->nxt[val];
+	}
+	cur->cnt++;
+}
+
+void set_fail(Node *head){
+	head->fail = head;
+	queue<Node*> q;
+	q.push(head);
+	while(!q.empty()){
+		Node *cur = q.front(); q.pop();
+		for(int i = 0; i < 26; i++){
+			Node *nxt = cur->nxt[i], *fail = cur->fail;
+			if(!nxt) continue;
+			while(fail != head && fail->nxt[i] == NULL)
+				fail = fail->fail;
+			if(cur != head && fail->nxt[i])
+				nxt->fail = fail->nxt[i];
+			else nxt->fail = head;
+			nxt->cnt += nxt->fail->cnt;
+			q.push(nxt);
+		}
+	}
+}
+
+char t[200001], str[200001];
 
 int main()
 {
+	int N; scanf("%s %d", t, &N);
+	L = strlen(t);
+	for(int i = 0; i < N; i++){
+		scanf("%s", str);
+		add(str, &nodes[0], 0, 1);
+		int len = strlen(str);
+		for(int j = (len - 1) / 2; j >= 0; j--)
+			swap(str[j], str[len - j - 1]);
+		add(str, &nodes[1], len - 1, -1);
+	}
+
+	set_fail(&nodes[0]);
+	set_fail(&nodes[1]);
+
+	Node *cur = &nodes[0];
+	for(int i = 0; i < L; i++){
+		int nval = t[i] - 'a';
+		while(cur != &nodes[0] && cur->nxt[nval] == NULL)
+			cur = cur->fail;
+		if(cur->nxt[nval]) cur = cur->nxt[nval];
+		ed[i] += cur->cnt;
+	}
+
+	cur = &nodes[1];
+	for(int i = L - 1; i >= 0; i--){
+		int nval = t[i] - 'a';
+		while(cur != &nodes[1] && cur->nxt[nval] == NULL)
+			cur = cur->fail;
+		if(cur->nxt[nval]) cur = cur->nxt[nval];
+		st[i] += cur->cnt;		
+	}
+
+	ll ans = 0;
+	for(int i = 1; i < L; i++)
+		ans += ed[i - 1] * st[i];
+	printf("%lld\n", ans);
 
 	return 0;
 }
